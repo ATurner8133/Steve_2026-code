fe0f48657f8fa6d5d1cb9e068d6dbc402c732b3c
therobcobots6729 <robcobots6729@gmail.com>
2026-01-20 18:11:48 -0500

    Updating vender deps

--- Steve2026/src/main/java/frc/robot/Constants.java
+++ Steve2026/src/main/java/frc/robot/Constants.java
@@ -17,7 +17,7 @@ public final class Constants {
   public static final double stickDeadband = 0.12;
        public static final int warningTime = 4000;
        public static final int errorTime = 7000;
-         public static final AprilTagFieldLayout fieldLayout = AprilTagFieldLayout.loadField(AprilTagFields.k2025ReefscapeAndyMark);
+         public static final AprilTagFieldLayout fieldLayout = AprilTagFieldLayout.loadField(AprilTagFields.k2026RebuiltAndymark);
   public static final class Swerve {
     
     // public static final boolean invertGyro = true; // Always ensure Gyro is CCW+
@@ -29,8 +29,8 @@ public final class Constants {
             COTSTalonFXSwerveConstants.SDS.MK4i.driveRatios.L1);
     
     /* Drivetrain Constants */
-    public static final double trackWidth = Units.inchesToMeters(26.5);
-    public static final double wheelBase = Units.inchesToMeters(20);
+    public static final double trackWidth = Units.inchesToMeters(21.625);
+    public static final double wheelBase = Units.inchesToMeters(21.625);
     public static final double wheelCircumference = chosenModule.wheelCircumference;
        // Elastic Notifications
 
